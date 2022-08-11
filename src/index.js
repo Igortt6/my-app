9fd8c28722e7eb91c492589310595450931238d1
Igor <exemple@gmail.com>
2022-08-11 10:59:30 +0300

    my first React steps

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const elem = (
+  <div>
+    <h2>Hello World</h2>
+    <input tupe="text" />
+    
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+elem,
 );
 
 // If you want to start measuring performance in your app, pass a function
